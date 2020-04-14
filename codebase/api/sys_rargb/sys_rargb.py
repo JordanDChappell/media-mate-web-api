9989ec317bac4e70980d9f148551176814fe980c
Jordan Chappell <jordan.chappell33@gmail.com>
2020-04-14 14:00:12 +1000

    Restructure sys-rargb routes and add python packages.

--- codebase/api/sys_rargb/sys_rargb.py
+++ codebase/api/sys_rargb/sys_rargb.py
@@ -1,13 +1,14 @@
 import os
 import sys
 from flask import Blueprint, request, jsonify
+import feedparser
 from utils.request_validation import required_query_params
 
 # Conditionally import the correct configuration for the running environment
 if os.getenv('FLASK_ENV') == 'development':
-    from dev_config import config
+  from dev_config import config
 else:
-    from config import config
+  from config import config
 
 # Define a blueprint object, using the flask blueprint function
 blueprint = Blueprint('sys_rargb', __name__)
@@ -28,27 +29,41 @@ about = {
   'environment': os.getenv('FLASK_ENV'),
 }
 
+########################
+###### API ROUTES ######
+########################
+
 # /{version}/sys/rargb/about
 # URI Parameters: N/A
 # Query Parameters: N/A
 @blueprint.route('{0}{1}/about'.format(version, baseUrl), methods=['GET'])
 def get_about():
-    """ Returns information about the API and environment """
-    return jsonify(about)
+  '''
+  Returns information about the API and environment
+  '''
+  return jsonify(about)
 
-# /{version}/sys/rargb/feeds
+# /{version}/sys/rargb/feeds/categories
 # URI Parameters: N/A
-# Query Parameters: 
-# - categories: type: list
-#               objects: number
-@blueprint.route('{0}{1}/feeds'.format(version, baseUrl), methods=['GET'])
-def get_feeds():
-    """ Retrieve RSS objects from RARGB """
-    message = required_query_params(request, ['categories'])
-    if message is None:
-      categories = request.args.get('categories')
-      return jsonify({
-        'categories': categories
-      })
-    else:
-      return message
+# Query Parameters: N/A
+@blueprint.route('{0}{1}/feeds/categories'.format(version, baseUrl), methods=['GET', 'PUT'])
+def feeds():
+  if request.method == 'PUT':
+    '''
+    Update the categories that sys-rargb is using to update the torrent list
+    '''
+    return jsonify({
+      'message': 'PUT request',
+      'data': request.get_json()
+    })
+  else:
+    '''
+    Return the list of categories currently being tracked
+    '''
+    return jsonify({
+      'message': 'GET request'
+    })
+
+########################
+####### API JOBS #######
+########################  
